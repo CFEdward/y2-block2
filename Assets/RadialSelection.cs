f745d7ee2bd35d548306c2d82c37b78c36878dee
amirabzv <bayazerovaa@gmail.com>
2024-11-30 17:36:43 +0100

    radial hand transform test

--- Assets/RadialSelection.cs
+++ Assets/RadialSelection.cs
@@ -4,6 +4,8 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.UI;
 using UnityEngine.UI;
+using System.Collections.Generic;
+
 
 public class RadialSelection : MonoBehaviour
 {
@@ -12,20 +14,63 @@ public class RadialSelection : MonoBehaviour
     public GameObject radialPartPrefab;
     public Transform radialPartCanvas;
     public float angleBetweenPart = 10;
+    public Transform handTransform;
+
+    private List<GameObject> spawnedParts = new List<GameObject>();
+    private int currentSelectedRadialPart = -1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnRadialPart();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnRadialPart();
+        GetSelectedRadialPart();
     }
 
+    public void GetSelectedRadialPart()
+    {
+        Vector3 centerToHand = handTransform.position - radialPartCanvas.position;
+        Vector3 centerToHandProojected = Vector3.ProjectOnPlane(centerToHand, radialPartCanvas.forward);
+
+        float angle = Vector3.SignedAngle(radialPartCanvas.up, centerToHandProojected, -radialPartCanvas.forward);
+
+        if (angle < 0)
+            angle += 360;
+
+        Debug.Log("ANGLE" +  angle);
+
+        currentSelectedRadialPart = (int) angle * numberOfRadialPart / 360;
+
+        for (int i = 0; i < spawnedParts.Count; i++)
+        {
+            if(i == currentSelectedRadialPart)
+            {
+                spawnedParts[i].GetComponent<Image>().color = Color.yellow;
+                spawnedParts[i].transform.localScale = 1.1f * Vector3.one;
+
+            }
+            else
+            {
+                spawnedParts[i].GetComponent<Image>().color = Color.white;
+                spawnedParts[i].transform.localScale = Vector3.one;
+            }
+        }
+    }
     public void SpawnRadialPart()
     {
+        foreach (var item in spawnedParts)
+        {
+            Destroy(item);
+        }
+
+        spawnedParts.Clear();
+
+
         for (int i = 0; i < numberOfRadialPart; i++)
         {
             float angle = i * 360 / numberOfRadialPart - angleBetweenPart/2;
@@ -36,6 +81,8 @@ public class RadialSelection : MonoBehaviour
             spawnedRadialpart.transform.localEulerAngles = radialPartEulerAngle;
 
             spawnedRadialpart.GetComponent<Image>().fillAmount = (1 / (float)numberOfRadialPart) - (angleBetweenPart/360);
+
+            spawnedParts.Add(spawnedRadialpart);
         }
     }
 }
