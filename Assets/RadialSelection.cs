ea6467a9f4b9fff8248232eb23af003b99c4d03a
amirabzv <bayazerovaa@gmail.com>
2024-11-30 18:24:29 +0100

    fixed direction

--- Assets/RadialSelection.cs
+++ Assets/RadialSelection.cs
@@ -5,16 +5,20 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.UI;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 
 public class RadialSelection : MonoBehaviour
 {
+    
     [Range(2,10)]
     public int numberOfRadialPart;
     public GameObject radialPartPrefab;
     public Transform radialPartCanvas;
     public float angleBetweenPart = 10;
     public Transform handTransform;
+
+    public UnityEvent<int> OnPartSelected;
 
     private List<GameObject> spawnedParts = new List<GameObject>();
     private int currentSelectedRadialPart = -1;
@@ -30,6 +34,12 @@ public class RadialSelection : MonoBehaviour
     {
         SpawnRadialPart();
         GetSelectedRadialPart();
+    }
+
+    public void HideAndTriggerSelected()
+    {
+        OnPartSelected.Invoke(currentSelectedRadialPart);
+        radialPartCanvas.gameObject.SetActive(false);
     }
 
     public void GetSelectedRadialPart()
@@ -73,7 +83,7 @@ public class RadialSelection : MonoBehaviour
 
         for (int i = 0; i < numberOfRadialPart; i++)
         {
-            float angle = i * 360 / numberOfRadialPart - angleBetweenPart/2;
+            float angle = - i * 360 / numberOfRadialPart - angleBetweenPart/2;
             Vector3 radialPartEulerAngle = new Vector3(0, 0, angle);
 
             GameObject spawnedRadialpart = Instantiate(radialPartPrefab, radialPartCanvas);
