1c5eef4ce27877ac5947dbf957a368145fdbac91
amirabzv <bayazerovaa@gmail.com>
2025-01-14 16:44:48 +0100

    Timer countdown

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("Component")]
+    public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")]
+    public float currentTime;
+    public bool countDown;
+
+    [Header("Limit Settings")]
+    public bool hasLimit;
+    public float timerLimit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+
+        if(hasLimit && ((countDown && currentTime <= timerLimit) || (!countDown && currentTime >= timerLimit)))
+        {
+            currentTime = timerLimit;
+            SetTimertext();
+            timerText.color = Color.red;
+            enabled = false;
+        }
+        SetTimertext();
+    }
+    private void SetTimertext()
+    {
+        timerText.text = currentTime.ToString("0");
+    }
+}
