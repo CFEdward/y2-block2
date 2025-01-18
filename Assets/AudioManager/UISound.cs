45cc224bc597a8909c9502ea6275cbaa81be7f98
Guillaume Maille <guillaume.maille.le@gmail.com>
2025-01-18 09:18:29 +0100

    Menu

---
+++ Assets/AudioManager/UISound.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISound : MonoBehaviour
+{
+    [SerializeField]
+    public Slider slider;
+    public string soundName = "ButtonPop";
+
+    public bool muting;
+    private AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FindObjectOfType<AudioManager>().Play("Sparks");
+    }
+
+    public void Volume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+    
+    //public void Pitch(float pitch)
+    //{
+    //    AudioListener.pitch = pitch;
+    //}
+
+    public void PlayClick()
+    {
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
+    }
+
+    public void MuteToggle(bool muted)
+    {
+        if (muted)
+        {
+            muting = true;
+            AudioListener.volume = 0;
+            Debug.Log("muting");
+        }
+
+        else
+        {
+            Debug.Log("NOT muting");
+            muting = false;
+            AudioListener.volume = 1;
+        }
+    }
+
+
+
+    public void PlayVolumeCheck(float value)
+    {
+        if (muting) {
+            return;
+                }
+        else {
+
+            if (audioManager != null)
+            {
+                FindObjectOfType<AudioManager>().Play("ButtonExample");
+            }
+            else
+            {
+                Debug.LogWarning("Cannot play sound: AudioManager is not assigned.");
+            }
+
+            Debug.Log($"Slider value changed: {value}");
+            FindObjectOfType<AudioManager>().Play("ButtonExample");
+        }
+    }
+
+
+}
